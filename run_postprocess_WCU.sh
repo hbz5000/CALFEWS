d94c47fbf1f87905b8a2540fed4a2471e8da2800
Andrew Hamilton <ahamilton144@gmail.com>
2024-03-10 20:54:22 +0000

    update wcu postprocess

--- run_postprocess_WCU.sh
+++ run_postprocess_WCU.sh
@@ -2,13 +2,14 @@
 
 results='results/WCU_results_bridges2/'
 
-#python3 combine_hdf5.py $results
+python3 combine_hdf5.py $results
 
-#python3 find_objs_WCU.py $results
+python3 find_objs_WCU.py $results
 
 grep label $results/objs_wcu.csv >> $results/objs_wcu_pareto.csv
 python3 ../pareto.py $results/objs_wcu.csv -o 43-46 -m 43-44 46 -e 2. 2. 5. 0.999 --output dum.csv --delimiter=", " --comment="#" --header=1
 cat dum.csv >> $results/objs_wcu_pareto.csv
+rm dum.csv
 
 cp $results/objs_wcu_pareto.csv $results/objs_wcu_pareto_withStatusQuo.csv
 for label in statusquo  
